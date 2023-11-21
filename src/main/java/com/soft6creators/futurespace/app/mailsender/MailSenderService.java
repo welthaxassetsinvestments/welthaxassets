b8da52a7fe38db889d6ea6ea11bb55c507b5a6e9
welthaxassets <148289025+welthaxassets@users.noreply.github.com>
2023-11-21 13:12:37 -0800

    Update MailSenderService.java

--- src/main/java/com/soft6creators/futurespace/app/mailsender/MailSenderService.java
+++ src/main/java/com/soft6creators/futurespace/app/mailsender/MailSenderService.java
@@ -19,7 +19,7 @@ public class MailSenderService {
 	public void sendEmail(String toEmail, String subject, String body) throws MessagingException, UnsupportedEncodingException {
 		MimeMessage message = mailSender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(message);
-		helper.setFrom("welthaxassets@gmail.com", "Welthax Assets");
+		helper.setFrom("digitalbasecryptominers@gmail.com", "Welthax Assets");
 		helper.setTo(toEmail);
 		helper.setSubject(subject);
 		helper.setText(body, true);
